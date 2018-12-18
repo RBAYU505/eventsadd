417dce3c2a4d96e1858c84e0af7b0396ba14cb7a
rbayu505 <restabayu.s@gmail.com>
2018-12-18 16:31:29 +0700

    tambah acara, tampilkan, edit, hapus , notification sender fixs

--- app/src/main/java/id/restabayu/eventsadd/ClassifiedEvent.java
+++ app/src/main/java/id/restabayu/eventsadd/ClassifiedEvent.java
@@ -7,15 +7,19 @@ public class ClassifiedEvent {
     private String deskripsi;
     private String tanggal;
     private String waktu;
+    private String selesai;
+    private String notif;
     private String nama;
 
-    public ClassifiedEvent(String eventId, String kategori, String tempat, String deskripsi, String tanggal, String waktu, String nama) {
+    public ClassifiedEvent(String eventId, String kategori, String tempat, String deskripsi, String tanggal, String waktu, String selesai, String notif, String nama) {
         this.eventId = eventId;
         this.kategori = kategori;
         this.tempat = tempat;
         this.deskripsi = deskripsi;
         this.tanggal = tanggal;
         this.waktu = waktu;
+        this.selesai = selesai;
+        this.notif = notif;
         this.nama = nama;
     }
     public ClassifiedEvent(){
@@ -46,6 +50,14 @@ public class ClassifiedEvent {
         this.waktu = waktu;
     }
 
+    public void setSelesai(String selesai) {
+        this.selesai = selesai;
+    }
+
+    public void setNotif(String notif) {
+        this.notif = notif;
+    }
+
     public void setNama(String nama) {
         this.nama = nama;
     }
@@ -72,6 +84,14 @@ public class ClassifiedEvent {
 
     public String getWaktu() {
         return waktu;
+    }
+
+    public String getSelesai() {
+        return selesai;
+    }
+
+    public String getNotif() {
+        return notif;
     }
 
     public String getNama() {
